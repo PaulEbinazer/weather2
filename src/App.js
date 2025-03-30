20d96360147b7240952ee36f68930b231cf392ea
PaulEbinazer <paulebinezer21@gmail.com>
2025-03-30 20:37:13 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { IoIosSearch } from "react-icons/io";
 
-function App() {
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {/* body */}
+
+      <div className="body">
+        <div className="searchbar">
+          <input type="text" placeholder="Enter the city name" />
+          <IoIosSearch className="i-1" />
+        </div>
+
+        {/* content */}
+
+        <div className="imgcontent">
+          <img src="./sun.png" alt="" />
+          <h1>Aruppukkottai</h1>
+          <h1>31 C</h1>
+        </div>
+
+        
+      </div>
     </div>
   );
-}
+};
 
 export default App;
